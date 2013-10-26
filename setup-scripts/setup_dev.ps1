6a2f71b915e7e049a326f8e06ced85fe5b0009e7
Dani <dani.meier@gmail.com>
2013-10-26 10:06:06 +0200

    Chocolatey-Scripts

---
+++ setup-scripts/setup_dev.ps1
@@ -0,0 +1,52 @@
+Set-ExecutionPolicy Unrestricted
+
+# Chocolatey
+iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))
+
+# Packages
+cinst msysgit
+cinst nodejs.install
+cinst putty
+cinst winscp
+cinst 7zip.install
+cinst nodejs.install
+cinst GoogleChrome
+cinst ChocolateyGUI
+cinst Console2
+cinst Firefox
+cinst sysinternals
+cinst fiddler4
+cinst ccleaner
+cinst AutoHotkey_l.install
+cinst ConEmu
+cinst linqpad4
+cinst launchy
+cinst beyondcompare
+cinst ilspy
+cinst sumatrapdf
+cinst SublimeText3
+cinst windirstat
+cinst vim
+cinst curl
+cinst Wget
+cinst IrfanView
+cinst SourceTree
+cinst SQLite
+cinst sqlite.shell
+cinst sqliteadmin
+cinst VisualStudio2013Ultimate
+cinst resharper
+#cinst ncrunch.vs2013
+cinst TotalCommander
+cinst SqlServer2012Express
+cinst poshgit
+
+# packages (non-vm)
+cinst VirtualCloneDrive
+cinst evernote
+cinst spotify
+cinst steam
+cinst jdownloader
+
+
+# posh-git

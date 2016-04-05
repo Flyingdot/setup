1b721718b1506d6c398c0081aa894dde5614d0cc
dani <dani.meier@gmail.com>
2016-04-05 08:52:34 +0200

    Rewrote install-script
     - Removed separate script for vm
     - Modify choco install-command to new syntax
     - Remove unused packages

--- setup-scripts/setup_dev.ps1
+++ setup-scripts/setup_dev.ps1
@@ -1,55 +1,45 @@
 Set-ExecutionPolicy Unrestricted
 
 # Chocolatey
-iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))
+iex ((new-object net.webclient).DownloadString('http://bit.ly/psChochoco install -yall'))
 
 # Packages
-cinst msysgit
-cinst nodejs.install
-cinst putty
-cinst winscp
-cinst 7zip.install
-cinst nodejs.install
-cinst GoogleChrome
-cinst ChocolateyGUI
-cinst Console2
-cinst Firefox
-cinst sysinternals
-cinst fiddler4
-cinst ccleaner
-cinst AutoHotkey_l.install
-cinst ConEmu
-cinst linqpad4
-cinst launchy
-cinst beyondcompare
-cinst ilspy
-cinst sumatrapdf
-cinst 1password
-cinst SublimeText3
-cinst windirstat
-cinst vim
-cinst curl
-cinst Wget
-cinst IrfanView
-cinst SourceTree
-cinst SQLite
-cinst sqlite.shell
-cinst sqliteadmin
-cinst VisualStudio2013Ultimate
-cinst resharper
-#cinst ncrunch.vs2013
-cinst TotalCommander
-cinst SqlServer2012Express
-cinst poshgit
-cinst pscx
-cinst python
+choco install -y git.install
+. $PROFILE
+choco install -y nodejs.install
+choco install -y 7zip.install
+choco install -y nodejs.install
+choco install -y GoogleChrome
+choco install -y Firefox
+choco install -y sysinternals
+choco install -y fiddler4
+choco install -y cmder
+choco install -y ConEmu
+choco install -y linqpad4
+choco install -y launchy
+choco install -y beyondcompare
+choco install -y ilspy
+choco install -y sumatrapdf
+choco install -y 1password
+choco install -y SublimeText3
+choco install -y windirstat
+choco install -y vim
+choco install -y curl
+choco install -y Wget
+choco install -y IrfanView
+choco install -y SourceTree
+choco install -y totalcommander
+choco install -y SqlServer2012Express
+choco install -y pscx
+choco install -y python
 
 # packages (non-vm)
-cinst VirtualCloneDrive
-cinst evernote
-cinst spotify
-cinst steam
-cinst jdownloader
+choco install -y VirtualCloneDrive
+choco install -y evernote
+choco install -y spotify
+choco install -y steam
 
-#nodejs packages
-npm install -g ifs
+# PSGet
+(new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
+. $PROFILE
+install-module posh-git

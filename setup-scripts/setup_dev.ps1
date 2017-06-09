92d8b522666515a9e2be82993f36380e46065ed5
Daniel <dani.meier@gmail.com>
2017-06-09 14:13:14 +0200

    Update packages

--- setup-scripts/setup_dev.ps1
+++ setup-scripts/setup_dev.ps1
@@ -7,15 +7,12 @@ choco install -y git.install
 . $PROFILE
 choco install -y nodejs.install
 choco install -y 7zip.install
-choco install -y nodejs.install
 choco install -y GoogleChrome
 choco install -y Firefox
 choco install -y sysinternals
 choco install -y fiddler4
 choco install -y cmder
-choco install -y ConEmu
-choco install -y linqpad4
-choco install -y launchy
+choco install -y linqpad
 choco install -y beyondcompare
 choco install -y ilspy
 choco install -y sumatrapdf
@@ -23,19 +20,11 @@ choco install -y 1password
 choco install -y SublimeText3
 choco install -y windirstat
 choco install -y vim-x64.install
-choco install -y curl
-choco install -y Wget
 choco install -y IrfanView
-choco install -y SourceTree
 choco install -y totalcommander
-choco install -y pscx
-choco install -y python
 
 # packages (non-vm)
 choco install -y VirtualCloneDrive
-choco install -y evernote
-choco install -y spotify
-choco install -y steam
 
 # PSGet
 (new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex

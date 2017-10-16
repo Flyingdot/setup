5d230aecd5ce106123e9ffc2e09cedea28751519
Dani Meier <dani.meier@swisscom.com>
2017-10-16 15:46:19 +0200

    Update package-list and install-process
     - use refreshenv
     - update env-paths

--- setup-scripts/setup_dev.ps1
+++ setup-scripts/setup_dev.ps1
@@ -1,10 +1,10 @@
 # Chocolatey
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 . $PROFILE
+refreshenv
 
 # Packages
 choco install -y git.install
-. $PROFILE
 choco install -y nodejs.install
 choco install -y 7zip.install
 choco install -y GoogleChrome
@@ -16,17 +16,28 @@ choco install -y linqpad
 choco install -y beyondcompare
 choco install -y ilspy
 choco install -y sumatrapdf
-choco install -y 1password
-choco install -y SublimeText3
 choco install -y windirstat
 choco install -y vim-x64.install
 choco install -y IrfanView
 choco install -y totalcommander
-
-# packages (non-vm)
+choco install -y visualstudiocode
+choco install -y skype
+choco install -y sysinternals
+choco install -y dropbox
+choco install -y python
+choco install -y everything
+choco install -y wox
+choco install -y dotnetcore-sdk
 choco install -y VirtualCloneDrive
+choco install -y virtualbox
+refreshenv
 
 # PSGet
 (new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
 . $PROFILE
 install-module posh-git
+refreshenv
+
+# Setup environment vars
+[Environment]::SetEnvironmentVariable("Path", $env:Path + ";c:\Program Files\Git\usr\bin\", [System.EnvironmentVariableTarget]::Machine)
+refreshenv

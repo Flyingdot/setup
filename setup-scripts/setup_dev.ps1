905cca2d27f266f23b4facfeded412558e4b95c1
Daniel <dani.meier@gmail.com>
2019-10-14 11:49:11 -0700

    Update setup_dev.ps1

--- setup-scripts/setup_dev.ps1
+++ setup-scripts/setup_dev.ps1
@@ -1,41 +1,48 @@
+Write-Host 'Set Explorer options'
+$key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
+Set-ItemProperty $key Hidden 1
+Set-ItemProperty $key HideFileExt 0
+Set-ItemProperty $key ShowSuperHidden 1
+Stop-Process -processname explorer
+
 # Chocolatey
+Write-Host 'Install Chocolatey'
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 . $PROFILE
 refreshenv
 
 # Packages
-choco install -y git.install
-choco install -y nodejs.install
-choco install -y 7zip.install
+Write-Host 'Install packages'
+choco install -y git
+choco install -y nodejs
+choco install -y 7zip
 choco install -y GoogleChrome
-choco install -y Firefox
+choco install -y firefox
 choco install -y sysinternals
 choco install -y fiddler4
-choco install -y cmder
 choco install -y linqpad
 choco install -y beyondcompare
 choco install -y ilspy
-choco install -y sumatrapdf.install
+choco install -y sumatrapdf
 choco install -y windirstat
-choco install -y vim-x64.install
 choco install -y IrfanView
 choco install -y totalcommander
-choco install -y visualstudiocode
-choco install -y skype
-choco install -y sysinternals
+choco install -y vscode
 choco install -y dropbox
 choco install -y python
 choco install -y everything
 choco install -y wox
-choco install -y docker
+choco install -y docker-desktop
 refreshenv
 
 # PSGet
+Write-Host 'Configure Powershell'
 (new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
 . $PROFILE
 install-module posh-git
 refreshenv
 
 # Setup environment vars
+Write-Host 'Set env-variables'
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";c:\Program Files\Git\usr\bin\", [System.EnvironmentVariableTarget]::Machine)
 refreshenv
